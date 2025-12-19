8574ab4a531bd8d3e5ecf1dc691e8de4ff50b996
omar owaiss <omarowaiss15@gmail.com>
2025-12-19 22:39:48 +0200

    Build a function that plays one round, logs the winner of the round and updates the score

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     let computerChoice = Math.random()
     if (computerChoice <= 0.3) {
@@ -22,5 +25,32 @@ function getHumanChoice() {
     }
     else  {
         return "scissors"
+    }
+}
+
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats scissors") 
+        humanScore++
+    }
+
+    else if (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock") 
+        humanScore++
+    }
+
+    else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats Paper") 
+        humanScore++
+    }
+
+    else if (humanChoice.toLowerCase() === computerChoice) {
+        console.log("It's a draw! Try again")
+    }
+
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        computerScore++
     }
 }
